5d72cbfd4bcc6684c5015f2ba7c8b9d4d31b7dc4
Ваше Имя <you@example.com>
2024-10-02 16:57:18 +0300

    create my_strcpy

--- string.cpp
+++ string.cpp
@@ -7,6 +7,7 @@ int my_strelen(char arr[]) {
     }
     return a;
 }
+
 int my_strcmp(char str1[], char str2[]) {
     int len1 = my_strelen(str1);
     int len2 = my_strelen(str2);
@@ -21,9 +22,21 @@ int my_strcmp(char str1[], char str2[]) {
     return 0;
 }
 
+void my_strcpy(char str1[], char str2[]) {
+    for(int i = 0; str2[i] != '\0'; i++) {
+        str1[i] = str2[i]; 
+    }
+    
+}
+
+void my_strcat(char str1[], char str2[]) {
+    
+}
+
 int main() {
-   char arr[] = "i love Mow";
-   char arr2[] = "i love Mow";
-   std::cout << my_strcmp(arr, arr2) << std::endl;
+    char arr[] = "i love spb";
+    char arr2[] = "i love Mowaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    my_strcpy(arr, arr2);
+    std::cout << arr << std::endl;
     return 0;
 }
