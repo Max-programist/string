93807f4f6b371d9517ed2b9a4b6f6a6826ab70d4
Ваше Имя <you@example.com>
2024-10-02 16:15:22 +0300

    improove strcpm

--- string.cpp
+++ string.cpp
@@ -7,18 +7,22 @@ int my_strelen(char arr[]) {
     }
     return a;
 }
-
 int my_strcmp(char str1[], char str2[]) {
     int len1 = my_strelen(str1);
     int len2 = my_strelen(str2);
     if(len1 != len2) {
         return 1;
     } 
+    for(int i = 0; i < len1; i++) {
+        if(str1[i] != str2[i]) {
+            return 1;
+        }
+    }
     return 0;
 }
 
 int main() {
-   char arr[] = "i love spb";
+   char arr[] = "i love Mow";
    char arr2[] = "i love Mow";
    std::cout << my_strcmp(arr, arr2) << std::endl;
     return 0;
